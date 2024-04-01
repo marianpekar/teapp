2bf39fa70d8232230804284032893fb6b7468230
Marian Pekár <marian.pekar@gmail.com>
2024-04-01 16:54:53 +0200

    Add wip drawer menu

--- app/src/main/java/com/marianpekar/teapp/activities/MainActivity.kt
+++ app/src/main/java/com/marianpekar/teapp/activities/MainActivity.kt
@@ -2,14 +2,20 @@ package com.marianpekar.teapp.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 import com.marianpekar.teapp.R
 import com.marianpekar.teapp.adapters.RecordsAdapter
 import com.marianpekar.teapp.data.RecordsStorage
@@ -17,8 +23,27 @@ import com.marianpekar.teapp.data.RecordsStorage
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recycler: RecyclerView
-    private lateinit var adapter : RecordsAdapter
-    private lateinit var records : RecordsStorage
+    private lateinit var adapter: RecordsAdapter
+    private lateinit var records: RecordsStorage
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+
+    inner class NavigationClicksListener : NavigationView.OnNavigationItemSelectedListener {
+        override fun onNavigationItemSelected(item: MenuItem): Boolean {
+            return onNavigationItemSelectedDelegate(item)
+        }
+
+        private fun onNavigationItemSelectedDelegate(item: MenuItem): Boolean {
+            when (item.itemId){
+                android.R.id.home -> toggleDrawer()
+                R.id.settings -> TODO()
+            }
+            return false
+        }
+    }
+
+    private val navigationClicksListener = NavigationClicksListener()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,14 +58,36 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        setNavigation()
+
         records = RecordsStorage(this@MainActivity)
 
         setRecycler()
         setAddNewRecordButton()
     }
 
-    private fun setRecycler()
-    {
+    private fun setNavigation() {
+        drawerLayout = findViewById(R.id.drawerLayout)
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.menu_open, R.string.menu_close)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener(navigationClicksListener)
+    }
+
+    private fun toggleDrawer(): Boolean {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return true
+    }
+
+    private fun setRecycler() {
         recycler = findViewById(R.id.recyclerRecords)
         recycler.layoutManager = LinearLayoutManager(this@MainActivity)
 
