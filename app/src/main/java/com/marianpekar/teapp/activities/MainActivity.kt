a26d5605ff60175c7193dcf19b4c0215f0ef5615
Marian Pekár <marian.pekar@gmail.com>
2024-04-01 17:44:38 +0200

    Remove unused import

--- app/src/main/java/com/marianpekar/teapp/activities/MainActivity.kt
+++ app/src/main/java/com/marianpekar/teapp/activities/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
