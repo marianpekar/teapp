40897972806e9af4a9e6c208f7b7b6d0e26291db
Marian Pekar <marian.pekar@gmail.com>
2024-04-03 00:26:26 +0200

    Move add new record button in main activity inside a toolbar

--- app/src/main/java/com/marianpekar/teapp/activities/MainActivity.kt
+++ app/src/main/java/com/marianpekar/teapp/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.marianpekar.teapp.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
@@ -13,7 +14,6 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.marianpekar.teapp.R
 import com.marianpekar.teapp.adapters.RecordsAdapter
@@ -73,24 +73,11 @@ class MainActivity : AppCompatActivityLocale() {
             insets
         }
 
-        setNavigation()
-
         records = RecordsStorage(this@MainActivity)
-
         setRecycler()
+
+        setDrawer()
         setAddNewRecordButton()
-    }
-
-    private fun setNavigation() {
-        drawerLayout = findViewById(R.id.drawerLayout)
-        val navigationView = findViewById<NavigationView>(R.id.navigationView)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
-        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.menu_open, R.string.menu_close)
-        drawerLayout.addDrawerListener(drawerToggle)
-        drawerToggle.syncState()
-
-        navigationView.setNavigationItemSelectedListener(navigationClicksListener)
     }
 
     private fun setRecycler() {
@@ -102,8 +89,20 @@ class MainActivity : AppCompatActivityLocale() {
         recycler.adapter = adapter
     }
 
+    private fun setDrawer() {
+        drawerLayout = findViewById(R.id.drawerLayout)
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.menu_open, R.string.menu_close)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener(navigationClicksListener)
+    }
+
     private fun setAddNewRecordButton() {
-        val addRecordButton: FloatingActionButton = findViewById(R.id.buttonAddRecord)
+        val addRecordButton: ImageButton = findViewById(R.id.buttonAddRecord)
 
         addRecordButton.setOnClickListener {
             val intent = Intent(this@MainActivity, AddRecordActivity::class.java)
