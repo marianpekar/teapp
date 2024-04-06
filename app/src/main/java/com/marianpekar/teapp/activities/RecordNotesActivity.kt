c44a1cdd861f67b9be90e886c463793451ecd61f
Marian Pekar <marian.pekar@gmail.com>
2024-04-06 23:49:21 +0200

    Change RecordNotesActivity to inherit from AppCompatActivityLocale

--- app/src/main/java/com/marianpekar/teapp/activities/RecordNotesActivity.kt
+++ app/src/main/java/com/marianpekar/teapp/activities/RecordNotesActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.marianpekar.teapp.R
 
-class RecordNotesActivity : AppCompatActivity() {
+class RecordNotesActivity : AppCompatActivityLocale() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
